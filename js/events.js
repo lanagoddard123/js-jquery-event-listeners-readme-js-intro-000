28fc0fba0a7abbcf2d69be2f0092461eff81e37c
lanasgoddard <lanasgoddard@gmail.com>
2019-02-26 09:25:25 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -5,6 +5,13 @@ function getIt(){
 
 }
 
+
+function frameIt(){
+  $("img").on("load",(e)=>{
+    var img1=document.querySelector('img');
+     img1.className="tasty"
+  })
+}
 $(document).ready(function(){
 
 // call functions here
